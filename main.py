3ffcf8cb921398b2976d386e470461d4b110a05d
Milky <107023633+MilkywayCocktail@users.noreply.github.com>
2022-07-25 10:50:40 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Draft by CAO
-# Last edit: 2022-07-20
+# Last edit: 2022-07-24
 from CSIKit.reader import get_reader
 from CSIKit.util import csitools
 from CSIKit.tools.batch_graph import BatchGraph
@@ -10,81 +10,114 @@ from CSIKit.util.filters import hampel
 
 import numpy as np
 import time
+import os
 
-class mycsi(object):
 
-    credit = 'cao'
+class MyException(Exception):
+    def __init__(self, catch):
+        self.catch = catch
+
+
+class PathError(MyException):
+    def __str__(self):
+        return "Please check the path\n" + \
+               "Current path is: " + str(self.catch) + \
+               "\nPlease run .set_path() to specify the path"
+
+
+class DataError(MyException):
+    def __str__(self):
+        return "Please run .load_data() to load data"
+
+
+class ArgError(MyException):
+    def __str__(self):
+        return "Please check the argument"
+
+
+class MyCsi(object):
+
+    __credit = 'cao'
 
     def __init__(self, name, path=None):
         self.name = name
         self.path = path
-        self.data = self.data()
-
+        self.data = self._Data(name)
 
     def set_path(self, path):
         self.path = path
-
+        print("path set")
 
     def load_data(self):
-        if self.path==None:
-            print("Please run .set_path() to specify a path")
+        try:
+            if self.path is None or not os.path.exists(self.path):
+                raise PathError(self.path)
 
-        else:
-            try:
-                print("load start - ", time.asctime(time.localtime(time.time())))
-                csi_reader = get_reader(self.path)
-                csi_data = csi_reader.read_file(self.path, scaled=True)
-                csi_amp, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric="amplitude")
-                csi_phase, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric="phase")
+            print(self.name, "load start - ", time.asctime(time.localtime(time.time())))
+            csi_reader = get_reader(self.path)
+            csi_data = csi_reader.read_file(self.path, scaled=True)
+            csi_amp, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric="amplitude")
+            csi_phase, no_frames, no_subcarriers = csitools.get_CSI(csi_data, metric="phase")
 
-                print("load complete - ", time.asctime(time.localtime(time.time())))
-                self.data.amp = csi_amp
-                self.data.phase = csi_phase
-                self.data.timestamps = csi_data.timestamps
+            print(self.name, "load complete - ", time.asctime(time.localtime(time.time())))
+            self.data.amp = csi_amp
+            self.data.phase = csi_phase
+            self.data.timestamps = csi_data.timestamps
 
-            except:
-                print("Please check the path")
-
+        except PathError as e:
+            print(e)
 
     def show_path(self):
-        if self.path==None:
-            print("Please run .set_path() to specify a path")
+        try:
+            if self.path is None:
+                raise PathError(self.path)
+            print(self.name, "path: ", self.path)
 
-        else:
-            print("path: ", self.path)
+        except PathError as e:
+            print(e)
 
-
-    class data:
-        def __init__(self):
+    class _Data:
+        def __init__(self, name):
+            self.name = name
             self.amp = None
             self.phase = None
             self.timestamps = None
 
-
         def show_shape(self):
             try:
+                if self.amp is None:
+                    raise DataError(self.amp)
+
                 items = ["no_frames=", "no_subcarriers=", "no_rx_ant=", "no_tx_ant="]
-                plist = [a + str(b) for a, b in zip(items, self.data.amp.shape)]
-                print("data shape: ", plist)
+                plist = [a + str(b) for a, b in zip(items, self.amp.shape)]
+                print(self.name, "data shape: ", *plist, sep='\n')
 
-            except:
-                print("Please run .load_data()")
-
+            except DataError as e:
+                print(e)
 
         def vis_all_rx(self, metric="amplitude"):
-            if metric=="amplitude":
-                csi_matrix = self.amp
-
-            elif metric=="phase":
-                csi_matrix = self.phase
-
             try:
+                if metric == "amplitude":
+                    csi_matrix = self.amp
+
+                elif metric == "phase":
+                    csi_matrix = self.phase
+
+                else:
+                    raise ArgError(metric)
+
+                if csi_matrix is None:
+                    raise DataError(self.amp)
+
                 for rx in range(csi_matrix.shape[2]):
                     csi_matrix_squeezed = np.squeeze(csi_matrix[:, :, rx, 0])
                     BatchGraph.plot_heatmap(csi_matrix_squeezed, self.timestamps)
 
-            except:
-                print("Please run .load_data()")
+            except ArgError as e:
+                print(e, '\n'+"Please specify \"amplitude\" or \"phase\"")
+
+            except DataError as e:
+                print(e)
 
 
     def aoa_by_music(self):
@@ -92,14 +125,15 @@ class mycsi(object):
         center_freq = 5.68e+09
         dist_antenna = 0.0264
 
+
 if __name__ == '__main__':
 
     mypath = "data/csi0720Atake6.dat"
 
 # CSI data composition: [no_frames, no_subcarriers, no_rx_ant, no_tx_ant]
 
-    today = mycsi("a6", mypath)
+    today = MyCsi("a6", mypath)
 
     today.load_data()
 
-    print(len(today.data.timestamps))
+    today.data.show_shape()
