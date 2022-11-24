574c12bcff294dd3b23ba02ed709a92ed8b3b512
Milky <brickcao@outlook.com>
2022-11-24 16:23:38 +0900

    Drafted interaction

--- sense.py
+++ sense.py
@@ -5,47 +5,131 @@ import time
 import os
 
 
-def depth2distance(frame):
+def depth2distance(depth_image, frame):
     dist_image = np.zeros_like(depth_image)
     for y in range(depth_image.shape[0]):
         for x in range(depth_image.shape[1]):
-            dist_image[y, x] = depth_frame.get_distance(x, y) * 100     # distance in cm
+            dist_image[y, x] = frame.get_distance(x, y) * 100     # distance in cm
     return dist_image
 
 
-pipeline = rs.pipeline()
-profile = pipeline.start()
+class MySense:
 
-index = 0
-img = 0
-t0 = int(round(time.time() * 1000))
+    def __init__(self, savepath):
+        self.length = 100
+        self.savepath = savepath
+        self.fps = 0
+        self.height = 480
+        self.width = 848
+        self.visual_output = True   # whether to save images as .jpg and timestamps as .txt
 
-path = '../sense/1124/take3/'
+        # used in session
+        self.pipeline = None
+        self.profile = None
 
-if not os.path.exists(path):
-    os.makedirs(path)
-timestamp = open(path + 'timestamps.txt', mode='w', encoding='utf-8')
-timestamp.write("Timestamps in miliseconds\n")
+    def setup(self, **kwargs):
 
-dmatrix= np.zeros((300, 480, 848))
+        if kwargs is not None:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
 
-for i in range(300):
+        self.pipeline = rs.pipeline()
+        self.profile = self.pipeline.start()
 
-    frames = pipeline.wait_for_frames()
-    depth_frame = frames.get_depth_frame()
-    t = int(round(time.time() * 1000)) - t0
-    depth_image = np.asanyarray(depth_frame.get_data())
-    transfer_image = depth_image // 100
+        if not os.path.exists(self.savepath):
+            os.makedirs(self.savepath)
 
-#    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    def run(self):
 
-    fname = str(i).zfill(5) + '.jpg'
-#    cv2.imwrite(path + 'img' + fname, depth_image)
-    cv2.imwrite(path + 'timg' + fname, transfer_image)
-    dmatrix[i] = depth_image
-    timestamp.write(str(t) + '\n')
-    print(i, "recorded")
+        while True:
+            print("Please enter a save name to start recording:\n",
+                  "- Enter length=<length> to change length\n",
+                  "- Enter exit to quit")
 
-pipeline.stop()
-np.save(path + "dmatrix.npy", dmatrix)
+            accept_string = input()
+
+            if accept_string == 'exit':
+                print("Exiting...")
+                self.pipeline.stop()
+                return
+
+            elif accept_string[:7] == 'length=':
+                if isinstance(eval(accept_string[7:]), int) and eval(accept_string[7:]) > 0:
+                    self.length = eval(accept_string[7:])
+                    print("Length changed to", self.length)
+
+            else:
+                name = accept_string
+                with self._Recorder(self.pipeline, self.savepath, self.visual_output) as r_session:
+                    r_session.record(name, self.length, self.height, self.width)
+
+    class _Recorder:
+
+        def __init__(self, pipeline, savepath, vis):
+            self.pipeline = pipeline
+            self.savepath = savepath
+            self.vis = vis
+
+        def __enter__(self):
+            print("Recording in process...")
+            return self
+
+        def record(self, name, length, height, width):
+            dmatrix = np.zeros((length, height, width))
+            timestamps = np.zeros(length)
+            timefile = None
+            img_path = None
+
+            if self.vis is True:
+                timefile = open(path + name + '_timestamps.txt', mode='a', encoding='utf-8')
+                timefile.write("Timestamps in miliseconds\n")
+                img_path = self.savepath + name + '/'
+                if not os.path.exists(img_path):
+                    os.makedirs(img_path)
+
+            t0 = int(round(time.time() * 1000))
+
+            for i in range(length):
+
+                frames = self.pipeline.wait_for_frames()
+                depth_frame = frames.get_depth_frame()
+                t = int(round(time.time() * 1000)) - t0
+                depth_image = np.asanyarray(depth_frame.get_data())
+
+                dmatrix[i] = depth_image
+                timestamps[i] = t
+
+                if self.vis is True:
+                    #    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03),
+                    #    cv2.COLORMAP_JET)
+
+                    img_name = str(i).zfill(5) + '.jpg'
+                    transfer_image = depth_image // 10
+                    #    cv2.imwrite(path + 'img' + fname, depth_image)
+                    cv2.imwrite(img_path + '_timg' + img_name, transfer_image)
+                    timefile.write(str(t))
+
+                print('\r', i + 1, "of", length, "recorded", end='')
+
+            np.save(self.savepath + name + "_dmatrix.npy", dmatrix)
+            np.save(self.savepath + name + "_timestamps.npy", timestamps)
+
+            if self.vis is True:
+                timefile.close()
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+
+            if exc_type is not None:
+                print(exc_type)
+            else:
+                print("\nRecording complete!")
+
+
+if __name__ == '__main__':
+
+    path = '../sense/1124/'
+
+    sense = MySense(path)
+    sense.setup()
+    sense.run()
 
