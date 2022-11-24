caa66047cc2e0701ea314e8b8b799eb2912b2ac5
Milky <brickcao@outlook.com>
2022-11-24 14:09:22 +0900

    Update sense.py

--- sense.py
+++ sense.py
@@ -20,31 +20,32 @@ index = 0
 img = 0
 t0 = int(round(time.time() * 1000))
 
-path = '../sense/1124/take1/'
+path = '../sense/1124/take3/'
 
 if not os.path.exists(path):
     os.makedirs(path)
 timestamp = open(path + 'timestamps.txt', mode='w', encoding='utf-8')
 timestamp.write("Timestamps in miliseconds\n")
 
-while index < 10:
+dmatrix= np.zeros((300, 480, 848))
+
+for i in range(300):
 
     frames = pipeline.wait_for_frames()
     depth_frame = frames.get_depth_frame()
     t = int(round(time.time() * 1000)) - t0
     depth_image = np.asanyarray(depth_frame.get_data())
-    transfer_image = depth_image // 10
+    transfer_image = depth_image // 100
 
 #    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-    index += 1
-
-    fname = str(img).zfill(5) + '.jpg'
-    cv2.imwrite(path + 'img' + fname, depth_image)
+    fname = str(i).zfill(5) + '.jpg'
+#    cv2.imwrite(path + 'img' + fname, depth_image)
     cv2.imwrite(path + 'timg' + fname, transfer_image)
+    dmatrix[i] = depth_image
     timestamp.write(str(t) + '\n')
-    print("save successful")
-    img += 1
+    print(i, "recorded")
 
 pipeline.stop()
+np.save(path + "dmatrix.npy", dmatrix)
 
