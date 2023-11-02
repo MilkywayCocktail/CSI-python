48abee51b192ac8c81e6fbb306e37c6f118f4ea1
Milky <brickcao@outlook.com>
2023-11-02 17:35:51 +0900

    complete

--- manipulate_image.py
+++ manipulate_image.py
@@ -56,29 +56,36 @@ class ImageGen:
     def __init__(self, name):
         self.name = name
         self.ind = []
-        self.raw_images = None
+        self.raw_imgs = None
         self.raw_bbx = None
-        self.gen_images = None
+        self.gen_imgs = None
         self.gen_bbx = None
 
     def load_images(self, path):
         print("Loading images...", end='')
-        self.raw_images = np.load(path)
-        self.raw_bbx = np.zeros((len(self.raw_images), 5))
+        self.raw_imgs = np.load(path)
+        self.raw_bbx = np.zeros((len(self.raw_imgs), 5))
         print("Complete!")
 
-    def show_images(self, select_num):
-        if self.raw_images:
-            inds = np.random.choice(list(range(len(self.raw_images))), select_num, replace=False)
-            inds = np.sort(inds)
+    def show_images(self, select_ind=None, select_num=8):
+        if self.raw_imgs is not None:
+            if select_ind:
+                inds = np.array(select_ind)
+            else:
+                inds = np.random.choice(list(range(len(self.raw_imgs))), select_num, replace=False)
+
             fig = plt.figure(constrained_layout=True)
             axes = fig.subplots(1, select_num)
+            if select_num == 1:
+                axes = [axes]
+                inds = [inds]
+            else:
+                inds = np.sort(inds)
             for j in range(len(axes)):
-                img = axes[j].imshow(self.raw_images[inds[j]], vmin=0, vmax=1)
+                img = axes[j].imshow(np.squeeze(self.raw_imgs[inds[j]]), vmin=0, vmax=1)
                 axes[j].axis('off')
                 axes[j].set_title(f"#{inds[j]}")
             fig.colorbar(img, ax=axes, shrink=0.8)
-
             plt.show()
         else:
             print("Please load images")
@@ -89,7 +96,7 @@ class ImageGen:
 
     def bounding_box(self, min_area=100, show=False):
         print("Labeling bounding boxes...", end='')
-        imgs = np.squeeze(self.raw_images)
+        imgs = np.squeeze(self.raw_imgs)
 
         for i in range(len(imgs)):
             img = np.squeeze(imgs[i]).astype('float32')
@@ -116,64 +123,105 @@ class ImageGen:
                                         (x + w, y + h),
                                         (0, 255, 0), 1)
                     if show:
-                        cv2.namedWindow('IMAGE', cv2.WINDOW_AUTOSIZE)
-                        cv2.imshow('IMAGE', img)
+                        cv2.namedWindow('Raw Image', cv2.WINDOW_AUTOSIZE)
+                        cv2.imshow('Raw Image', img)
                         key = cv2.waitKey(33) & 0xFF
                         if key == ord('q'):
                             break
         print("Complete!")
 
     def generate_imgs(self, Bx=None, By=None, HW=None, select_ind=None):
-        blank_image = np.zeros((128, 128))
         if select_ind:
             ind = [select_ind]
         else:
             ind = self.ind
 
-        for i in ind:
-            print(f"Generating {i} of {ind}...")
-            x, y, w, h, d = self.raw_bbx[i]
-            subject = self.raw_images[i][y:y + h, x:x + w]
-            generated_images = np.zeros((1, 128, 128))
-            generated_bbx = np.zeros((1, 5))
+        if ind:
+            for i in ind:
+                print(f"Generating {i} of {ind}...")
+                x, y, w, h, d = self.raw_bbx[i]
+                x, y, w, h = int(x), int(y), int(w), int(h)
+                subject = np.squeeze(self.raw_imgs[i])[y:y + h, x:x + w]
 
-            if HW:
-                subject = cv2.resize(subject, (w*HW, h*HW), interpolation=cv2.INTER_AREA)
-                (h, w) = subject.shape
-            if Bx:
-                Bxs = np.floor(np.arange(0, 128 - w, Bx))
-                for Bxi in Bxs:
-                    image = blank_image
-                    image[y:y + h, Bxi:Bxi + w] = subject
-                    bbx = np.array([Bxi, y, w, h, d])
-                    np.concatenate((generated_images, image), axis=0)
-                    np.concatenate((generated_bbx, bbx), axis=0)
+                plt.imshow(subject)
+                plt.show()
+                generated_images = np.zeros((1, 128, 128))
+                generated_bbx = np.zeros((1, 5))
 
-            if By:
-                Bys = np.floor(np.arange(0, 128 - h, By))
-                for Byi in Bys:
-                    image = blank_image
-                    image[Byi:Byi + h, x:x + w] = subject
-                    bbx = np.array([x, Byi, w, h, d])
-                    np.concatenate((generated_images, image), axis=0)
-                    np.concatenate((generated_bbx, bbx), axis=0)
+                if HW:
+                    subject = cv2.resize(subject, (w*HW, h*HW), interpolation=cv2.INTER_AREA)
+                    (h, w) = subject.shape
+                if Bx:
+                    Bxs = np.floor(np.arange(0, 128 - w, Bx)).astype(int)
+                    print(f"{len(Bxs)} images to generate")
+                    for Bxi in Bxs:
+                        image = np.zeros((128, 128))
+                        image[y:y + h, Bxi:Bxi + w] = subject
+                        bbx = np.array([Bxi, y, w, h, d])
+                        generated_images = np.concatenate((generated_images, image.reshape((1, 128, 128))), axis=0)
+                        generated_bbx = np.concatenate((generated_bbx, bbx.reshape(1, 5)), axis=0)
 
-            np.delete(generated_images, 0, axis=0)
+                if By:
+                    Bys = np.floor(np.arange(0, 128 - h, By)).astype(int)
+                    print(f"{len(Bys)} images to generate")
+                    for Byi in Bys:
+                        image = np.zeros((128, 128))
+                        image[Byi:Byi + h, x:x + w] = subject
+                        bbx = np.array([x, Byi, w, h, d])
+                        generated_images = np.concatenate((generated_images, image.reshape((1, 128, 128))), axis=0)
+                        generated_bbx = np.concatenate((generated_bbx, bbx.reshape(1, 5)), axis=0)
 
-            if not self.gen_images:
-                self.gen_images = generated_images
-            else:
-                self.gen_images = np.concatenate((self.gen_images, generated_images), axis=0)
+                generated_images = np.delete(generated_images, 0, axis=0)
+                generated_bbx = np.delete(generated_bbx, 0, axis=0)
 
-        print("Generation complete!")
+                if not self.gen_imgs:
+                    self.gen_imgs = generated_images
+                else:
+                    self.gen_imgs = np.concatenate((self.gen_imgs, generated_images), axis=0)
+                if not self.gen_bbx:
+                    self.gen_bbx = generated_bbx
+                else:
+                    self.gen_bbx = np.concatenate((self.gen_bbx, generated_bbx), axis=0)
+
+            print("Generation complete!")
+        else:
+            print("Please specify an index!")
+
+    def view_gen(self):
+        for i in range(len(self.gen_imgs)):
+            img = np.squeeze(self.gen_imgs[i])
+            plt.imshow(img)
+            plt.show()
+
+    def view_generation(self):
+        if self.gen_imgs is not None:
+            print(f"Viewing generated {self.gen_imgs.shape[0]} images...")
+
+            for i in range(len(self.gen_imgs)):
+                x, y, w, h, d = self.gen_bbx[i]
+                x, y, w, h = int(x), int(y), int(w), int(h)
+                img = np.squeeze(self.gen_imgs[i]).astype('float32')
+                img = cv2.rectangle(cv2.cvtColor(img, cv2.COLOR_GRAY2BGR),
+                                    (x, y),
+                                    (x + w, y + h),
+                                    (0, 255, 0), 1)
+                cv2.namedWindow('Generated Image', cv2.WINDOW_AUTOSIZE)
+                cv2.imshow('Generated Image', img)
+
+                key = cv2.waitKey(33) & 0xFF
+                if key == ord('q'):
+                    break
+
+        else:
+            print("No generated images!")
 
     def save(self, save_path):
         if not os.path.exists(save_path):
             os.makedirs(save_path)
         if self.raw_bbx:
             np.save(self.raw_bbx, f"{save_path}{self.name}_raw_bbx.npy")
-        if self.gen_images:
-            np.save(self.gen_images, f"{save_path}{self.name}_gen_images.npy")
+        if self.gen_imgs:
+            np.save(self.gen_imgs, f"{save_path}{self.name}_gen_imgs.npy")
         if self.gen_bbx:
             np.save(self.gen_bbx, f"{save_path}{self.name}_gen_bbx.npy")
         print("All saved!")
@@ -183,5 +231,8 @@ if __name__ == '__main__':
 
     gen = ImageGen("01")
     gen.load_images("../dataset/0509/make01-finished/img.npy")
-    gen.bounding_box(show=True)
+    gen.bounding_box(show=False)
+    gen.show_images(select_ind=250, select_num=1)
+    gen.generate_imgs(Bx=10, select_ind=250)
+    gen.view_generation()
     #bounding_box('../dataset/0725/make00-finished/img.npy')
--- test.py
+++ test.py
@@ -36,26 +36,3 @@ def labels(path):
                 print(eval(line.split(',')[2][2:]))
                 #label.append([eval(line.split(',')[0]), eval(line.split(',')[1])])
 
-
-e = 2.173520803451538
-l= [-1.64485363e+00, -1.08031934e+00, -7.38846849e-01, -4.67698799e-01,
- -2.27544977e-01, -1.39145821e-16,  2.27544977e-01,  4.67698799e-01,
-  7.38846849e-01,  1.08031934e+00,  1.64485363e+00]
-
-#print(np.searchsorted(l, e))
-
-x = np.linspace(0, 3 * np.pi, 500)
-y = np.sin(x)
-
-x1 = x[:int(500/3)]
-x2 = x[int(500/3):int(500/3)*2]
-x3 = x[int(500/3)*2:]
-
-y1 = np.sin(x1)
-y2 = np.sin(x2)
-y3 = np.sin(x3)
-
-plt.plot(x1,y1)
-plt.plot(x2,y2,linestyle = '-.')
-plt.plot(x3,y3,linestyle = ':')
-#plt.show()
