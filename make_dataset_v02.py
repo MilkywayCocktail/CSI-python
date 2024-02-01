a322243d6ac88e416547804e9ee674be18c667a7
Milky <brickcao@outlook.com>
2024-02-01 21:21:38 +0900

    Update make_dataset_v02.py

--- make_dataset_v02.py
+++ make_dataset_v02.py
@@ -362,8 +362,7 @@ class MyDataMaker(BagLoader, CSILoader, LabelParser):
         boundary = [-1, -1, -1]
         for i in tqdm(range(self.frames)):
             csi_index = np.searchsorted(self.csi.timestamps,
-                                        self.result['vanilla']['time'][i])[0]
-            print(csi_index)
+                                        self.result['vanilla']['time'][i, 0, 0])
             self.result['vanilla']['ind'][i, ...] = csi_index
             try:
                 if self.alignment == 'head' and csi_index > boundary[2]:
