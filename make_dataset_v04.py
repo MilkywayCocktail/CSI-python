b3d740d7418f58fc88636d97153eb767417a9246
Milky <brickcao@outlook.com>
2024-04-19 16:34:02 +0900

    Update make_dataset_v04.py

--- make_dataset_v04.py
+++ make_dataset_v04.py
@@ -8,6 +8,7 @@ from tqdm.notebook import tqdm
 import matplotlib.pyplot as plt
 from datetime import datetime
 from IPython.display import display, clear_output
+from functools import wraps
 
 
 ##############################################################################
@@ -22,8 +23,20 @@ from IPython.display import display, clear_output
 # -------------------------------------------------------------------------- #
 ##############################################################################
 
+def wrapper(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        print(f"Starting {func.__name__}...")
+        ret = func(*args, **kwargs)
+        print('Done')
+        return ret
+
+    return inner
+
+
 class LabelParser:
-    def __init__(self, label_path, *args, **kwargs):
+    def __init__(self, name, label_path, *args, **kwargs):
+        self.name = name
         self.label_path: str = label_path
         self.labels: dict = {}
         if self.label_path:
@@ -40,7 +53,7 @@ class LabelParser:
                 |-other attributes\n
         :return: Parsed label
         """
-        print('Loading label...', end='')
+        print(f'{self.name} loading label...', end='')
         label: dict = {}
         with open(self.label_path) as f:
             for i, line in enumerate(f):
@@ -71,7 +84,7 @@ class LabelParser:
         print('Done')
 
     def save_labels(self):
-        print("Saving labels...", end='')
+        print(f"{self.name} saving labels...", end='')
         np.save(f"{os.path.split(self.label_path)[0]}/{os.path.split(self.label_path)[1]}.npy", self.labels)
         print("Done")
 
@@ -79,7 +92,8 @@ class LabelParser:
 class ImageLoader:
     # Camera time should have been calibrated
 
-    def __init__(self, img_path, camera_time_path, img_shape, *args, **kwargs):
+    def __init__(self, name, img_path, camera_time_path, img_shape, *args, **kwargs):
+        self.name = name
         self.img_path = img_path
         self.camera_time_path = camera_time_path
         self.img, self.camera_time = self.load_img()
@@ -90,14 +104,14 @@ class ImageLoader:
         self.c_img = None
 
     def load_img(self):
-        print('Loading IMG...')
+        print(f'{self.name} loading IMG...')
         img = np.load(self.img_path)
         camera_time = np.load(self.camera_time_path)
         print(f" Loaded images of {img.shape} as {img.dtype}")
         return img, camera_time
 
     def depth_mask(self, threshold=0.5):
-        tqdm.write("Masking...")
+        tqdm.write(f"{self.name} masking...")
         median = np.median(np.squeeze(self.img), axis=0)
         threshold = median * threshold
         plt.imshow(threshold / np.max(threshold))
@@ -106,17 +120,16 @@ class ImageLoader:
         for i in tqdm(range(len(self.img))):
             mask = np.squeeze(self.img[i]) < threshold
             self.img[i] *= mask
-        tqdm.write("Done")
 
     def convert_img(self, threshold=3000):
-        print('Converting IMG...', end='')
+        print(f'Converting IMG...', end='')
         self.img[self.img > threshold] = threshold
         self.img /= float(threshold)
         print('Done')
 
     def crop(self, min_area=0, show=False):
         """
-        Calculete cropped images, bounding boxes, center coordinates, depth.\n
+        Calculate cropped images, bounding boxes, center coordinates, depth.\n
         :param min_area: 0
         :param show: whether show images with bounding boxes
         :return: bbx, center, depth, c_img
@@ -147,12 +160,12 @@ class ImageLoader:
                     non_zero = (patch != 0)
                     average_depth = patch.sum() / non_zero.sum()
                     self.bbx[i] = x, y, w, h
-                    self.center[i] = int(x + w/2), int(y + h/2)
+                    self.center[i] = int(x + w / 2), int(y + h / 2)
                     self.depth[i] = average_depth
 
                     subject = np.squeeze(self.img[i])[y:y + h, x:x + w]
                     image = np.zeros((128, 128))
-                    image[int(64 - h/2):int(64 + h/2), int(64 -  w/2):int(64 + w/2)] = subject
+                    image[int(64 - h / 2):int(64 + h / 2), int(64 - w / 2):int(64 + w / 2)] = subject
                     self.c_img[i, 0] = image
 
                     if show:
@@ -166,12 +179,29 @@ class ImageLoader:
                         if key == ord('q'):
                             break
 
+    def convert_bbx(self, w_scale=226, h_scale=128):
+        print('Converting bbx...', end='')
+
+        self.bbx[:, 0, 0] /= float(w_scale)
+        self.bbx[:, 0, 2] /= float(w_scale)
+        self.bbx[:, 0, 1] /= float(h_scale)
+        self.bbx[:, 0, 3] /= float(h_scale)
+
+        print('Done')
+
+    def convert_depth(self, threshold=3000):
+        print('Converting depth...', end='')
+        self.depth[self.depth > threshold] = threshold
+        self.depth /= float(threshold)
+        print('Done')
+
 
 class CSILoader:
     # CSI time should have been calibrated
     # CSI timestamps inside csi
 
-    def __init__(self, csi_path, csi_configs: pycsi.MyConfigs, *args, **kwargs):
+    def __init__(self, name, csi_path, csi_configs: pycsi.MyConfigs, *args, **kwargs):
+        self.name = name
         self.csi_path = csi_path
         self.csi_configs = csi_configs
         self.csi = self.load_csi()
@@ -194,7 +224,7 @@ class CSILoader:
 
 class MyDataMaker(ImageLoader, CSILoader, LabelParser):
     def __init__(self,
-                 name = None,
+                 name=None,
                  csi_shape: tuple = (2, 100, 30, 3),
                  alignment: str = 'tail',
                  jupyter_mode=False,
@@ -228,7 +258,8 @@ class MyDataMaker(ImageLoader, CSILoader, LabelParser):
         self.train_pick = None
         self.test_pick = None
 
-    def reshape_csi(self, csi, filter=True, pd=True):
+    @staticmethod
+    def reshape_csi(csi, filter=True, pd=True):
         """
         Reshape CSI into channel * sub * packet.\n
         :return: reshaped (+ filtered) CSI
@@ -256,7 +287,6 @@ class MyDataMaker(ImageLoader, CSILoader, LabelParser):
 
             aoatof = np.zeros((length, 33))
             for i, packet in enumerate(csi_):
-
                 u1, s1, v1 = np.linalg.svd(packet.transpose(3, 2, 1, 0).
                                            reshape(-1, rx, sub * length), full_matrices=False)
                 aoa = np.angle(u1[:, 0, 0].conj() * u1[:, 1, 0]).squeeze()
@@ -300,6 +330,8 @@ class MyDataMaker(ImageLoader, CSILoader, LabelParser):
                         self.labels[gr][seg][i]: dict = {}
                     else:
                         # Sample-level ids
+                        # img: 1 index
+                        # CSI: list of indices
                         self.labels[gr][seg][i] = {'img': img_id[i],
                                                    'csi': sample_csi_id}
                 # Segment-level ids
@@ -351,7 +383,7 @@ class MyDataMaker(ImageLoader, CSILoader, LabelParser):
             self.test_pick = de_picks
         print('Done')
 
-    def export_data(self, filter=True):
+    def export_data(self, filter=True, pd=True):
         """
          Find csi, image and timestamps according to label.\n
         :param filter: whether apply savgol filter on each CSI sample
@@ -361,10 +393,10 @@ class MyDataMaker(ImageLoader, CSILoader, LabelParser):
                       'csi': (len(self.train_pick), *self.csi_shape),
                       'time': (len(self.train_pick), 1, 1),
                       'cimg': (len(self.train_pick), 1, 128, 128),
-                      'center': (len(self.train_pick), 1, 1),
+                      'center': (len(self.train_pick), 1, 2),
                       'depth': (len(self.train_pick), 1, 1),
-                      'pd': (len(self.train_pick), 1, 33)
-        }
+                      'pd': (len(self.train_pick), 100, 33)
+                      }
 
         for mod, shape in modalities.items():
             self.train_data[mod] = np.zeros(shape)
@@ -373,18 +405,34 @@ class MyDataMaker(ImageLoader, CSILoader, LabelParser):
         tqdm.write('Exporting train data...')
         for i in tqdm(range(len(self.train_pick))):
             gr, seg, sample = self.train_pick[i].items()
-            self.train_data['img'][i] = self.img[self.labels[gr][seg][sample]['img']]
-            self.train_data['csi'][i], self.train_data['pd'][i]= self.reshape_csi(self.img[self.labels[gr][seg][sample]['csi']], filter=filter)
+            self.train_data['rimg'][i] = self.img[self.labels[gr][seg][sample]['img']]
+            self.train_data['cimg'][i] = self.c_img[self.labels[gr][seg][sample]['img']]
+            self.train_data['center'][i] = self.center[self.labels[gr][seg][sample]['img']]
+            self.train_data['depth'][i] = self.depth[self.labels[gr][seg][sample]['img']]
             self.train_data['time'][i] = self.camera_time[self.labels[gr][seg][sample]['img']]
+
+            (self.train_data['csi'][i],
+             self.train_data['pd'][i]) = self.reshape_csi(self.csi.csi[self.labels[gr][seg][sample]['csi']],
+                                                          filter=filter, pd=pd)
+        for mod, value in self.train_data.items():
+            tqdm.write(f"Exproted train: {mod} of {value.shape} as {value.dtype}")
 
         tqdm.write('Exporting test data...')
         for i in tqdm(range(len(self.test_pick))):
             gr, seg, sample = self.test_pick[i].items()
-            self.train_data['img'][i] = self.img[self.labels[gr][seg][sample]['img']]
-            self.train_data['csi'][i] = self.reshape_csi(self.img[self.labels[gr][seg][sample]['csi']], filter=filter)
-            self.train_data['time'][i] = self.camera_time[self.labels[gr][seg][sample]['img']]
+            self.test_data['rimg'][i] = self.img[self.labels[gr][seg][sample]['img']]
+            self.test_data['cimg'][i] = self.c_img[self.labels[gr][seg][sample]['img']]
+            self.test_data['center'][i] = self.center[self.labels[gr][seg][sample]['img']]
+            self.test_data['depth'][i] = self.depth[self.labels[gr][seg][sample]['img']]
+            self.test_data['time'][i] = self.camera_time[self.labels[gr][seg][sample]['img']]
 
-    def save_dataset(self):
+            (self.test_data['csi'][i],
+             self.test_data['pd'][i]) = self.reshape_csi(self.csi.csi[self.labels[gr][seg][sample]['csi']],
+                                                         filter=filter, pd=pd)
+        for mod, value in self.test_data.items():
+            tqdm.write(f"Exproted test: {mod} of {value.shape} as {value.dtype}")
+
+    def save_data(self):
         print("Saving...", end='')
         if not os.path.exists(self.save_path):
             os.makedirs(self.save_path)
@@ -394,41 +442,105 @@ class MyDataMaker(ImageLoader, CSILoader, LabelParser):
                     self.train_data)
             np.save(os.path.join(self.save_path, f"{self.name}_csilen{self.csi_shape[1]}_{modality}_test.npy"),
                     self.test_data)
-
+        np.save(os.path.join(self.save_path, f"{self.name}_csilen{self.csi_shape[1]}_label_train.npy"),
+                self.train_pick)
+        np.save(os.path.join(self.save_path, f"{self.name}_csilen{self.csi_shape[1]}_label_test.npy"),
+                self.test_pick)
         print("Done")
 
 
 class DatasetMaker:
     version = 'V04'
 
-    def __init__(self, subs, configs,):
-        self.subs = subs
-        subs = [
-            ('01', 5400),
-            ('02', 5000),
-            ('03', 5100),
-            ('04', 5100)
-        ]
+    def __init__(self, subs=None, jupyter=True, mode='normal', dataset_name='None'):
+        if subs is None:
+            subs = ['01', '02', '03', '04']
 
+        self.subs = subs
         configs = pycsi.MyConfigs()
         configs.tx_rate = 0x1c113
         configs.ntx = 3
 
-        for (name, length) in subs:
-            mkdata = MyDataMaker(csi_configs=configs, img_shape=(226, 128), csi_shape=(2, 900, 30, 3),
-                                 total_frames=length, assemble_number=1,
-                                 img_path=f"../sense/0509/raw226_128/{name}_img.npy",
-                                 camera_time_path=f"../sense/0509/raw226_128/{name}_camtime.npy",
-                                 csi_path=f"../npsave/0509/0509A{name}-csio.npy",
-                                 label_path=f"../sense/0509/{name}_labels.csv",
-                                 save_path=f"../dataset/0509/make16_900/")
-            mkdata.jupyter = True
+        self.configs = configs
+        self.jupyter = jupyter
+        self.mode = mode
+        self.dataset_name = dataset_name
+        self.train_data = []
+        self.test_data = []
+        self.train_data_final: dict = {}
+        self.valid_data_final: dict = {}
+        self.test_data_final: dict = {}
+
+        self.train_length = 0
+        self.valid_length = 0
+        self.test_length = 0
+
+        self.valid_mask = None
+        self.valid_ind = None
+        self.tv_ind = None
+
+        self.modalities = ('rimg', 'csi', 'time', 'cimg', 'center', 'depth', 'pd')
+
+    def make_data(self):
+        for sub in self.subs:
+            mkdata = MyDataMaker(csi_configs=self.configs, img_shape=(226, 128), csi_shape=(2, 900, 30, 3),
+                                 img_path=f"../sense/0509/raw226_128/{sub}_img.npy",
+                                 camera_time_path=f"../sense/0509/raw226_128/{sub}_camtime.npy",
+                                 csi_path=f"../npsave/0509/0509A{sub}-csio.npy",
+                                 label_path=f"../sense/0509/{sub}_labels.csv",
+                                 save_path=f"../dataset/0509/{self.dataset_name}_{self.mode}/")
+            mkdata.jupyter = self.jupyter
             mkdata.csi.extract_dynamic(mode='overall-divide', ref='tx', ref_antenna=1)
             mkdata.csi.extract_dynamic(mode='highpass')
             mkdata.convert_img()
-            mkdata.export_data(filter=True, alignment='tail')
-            mkdata.deoverlap()
-            mkdata.slice_by_label()
-            mkdata.assemble()
-            mkdata.save_dataset(name + '_rimg', 'img')
-            mkdata.save_dataset(name + '_dyn', 'csi', 'time', 'csi_ind', 'label')
+            mkdata.crop()
+            mkdata.convert_bbx()
+            mkdata.convert_depth()
+            mkdata.pick_samples(alignment='tail')
+            mkdata.divide_train_test(mode=self.mode)
+            mkdata.export_data(filter=True)
+            self.train_data.append(mkdata.train_data)
+            self.test_data.append(mkdata.test_data)
+            mkdata.save_data()
+
+    def regroup_data(self):
+        print("Regrouping...")
+        for modality in self.modalities:
+            self.train_data_final[modality] = np.concatenate([train[modality] for train in self.train_data])
+            self.test_data_final[modality] = np.concatenate([test[modality] for test in self.test_data])
+            print(f"{modality} total train length = {len(self.train_data_final[modality])}, "
+                  f"total test length = {len(self.test_data_final[modality])}")
+            self.train_length = len(self.train_data_final[modality])
+            self.test_length = len(self.test_data_final[modality])
+
+    def divide_valid(self):
+        """
+        Divide validation set from training set by ratio of 0.2.\n
+        :return: Finalized train, valid, test sets
+        """
+        print("Dividing valid...")
+        self.tv_ind = np.arange(self.train_length).astype(int)
+        valid_size = int(self.train_length * 0.2)
+        self.valid_ind = np.random.choice(self.tv_ind, valid_size, replace=False)
+        self.valid_mask = np.ones(self.train_length, np.bool)
+        self.valid_mask[self.valid_ind] = 0
+
+        print(f"Divided train length = {self.train_length - valid_size}, "
+              f"valid length = {valid_size}, test length = {self.test_length}")
+
+    def save(self):
+        tqdm.write("Saving...")
+        save_path = f"../dataset/0509/{self.dataset_name}_{self.mode}_split/"
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        for mod in self.modalities:
+            print(f" Saving {mod}...")
+            np.save(f"{save_path}{mod}_train.npy", self.train_data_final[mod][self.valid_mask])
+            np.save(f"{save_path}{mod}_valid.npy", self.train_data_final[mod][self.valid_ind])
+            np.save(f"{save_path}{mod}_test.npy", self.test_data_final[mod])
+        np.save(f"{save_path}ind_train.npy", self.tv_ind[self.valid_mask])
+        np.save(f"{save_path}ind_valid.npy", self.valid_ind)
+
+        print("All saved!")
+        
